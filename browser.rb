b1ee68546609f2f3dd8722931907b3f203315332
Harry Brodsky <hbrodsk1@binghamton.edu>
2016-06-20 11:27:06 -0400

    Update server and browser files. Add thanks.html

--- browser.rb
+++ browser.rb
@@ -1,7 +1,7 @@
 require 'socket'
 
-host = 'www.tutorialspoint.com'
-port = 80
+host = 'localhost'
+port = 2000
 path = "/index.html"
 
 request = "GET #{path} HTTP/1.0\r\n\r\n"
@@ -10,4 +10,6 @@ socket = TCPSocket.open(host, port)
 socket.print(request)
 response = socket.read
 headers,body = response.split("\r\n\r\n", 2)
+puts headers
 puts body
+socket.close
--- web_server.rb
+++ web_server.rb
@@ -2,9 +2,31 @@
 require 'socket'
 
 server = TCPServer.open(2000)
-loop {
+loop {  #server runs forever
 	client = server.accept
-	client.puts(Time.now.ctime)
-	client.puts "Closing the connection. Bye!"
+
+	#loops until whole header is grabbed
+	header = ""
+	while line = client.gets
+		header << line
+		break if header =~ /\r\n\r\n$/
+	end
+
+	#parses out method and path from status line
+	method = header.split[0]
+	path = header.split[1][1..-1]
+
+	if method == "GET" && File.exist?(path)
+		response_body = (File.open(path, 'r')).read
+    	response_head = "HTTP/1.0 200 OK\nContent-Length: #{response_body.length}\r\n\r\n"
+	else
+    	response_head = ("HTTP/1.0 404 Not Found\r\n\r\n")
+    	response_body = ("Sorry, file not found.")
+	end
+
+
+
+	client.puts(response_head)
+  	client.puts(response_body)
 	client.close
 }
